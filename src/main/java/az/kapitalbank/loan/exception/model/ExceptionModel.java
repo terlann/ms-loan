52c818ddd09de8d39000e365caa36ad3c5d18a30
MikayilovOE <orxan.mikayilov@kapitalbank.az>
2022-02-23 12:38:30 +0400

    added exception handler added some validations

--- src/main/java/az/kapitalbank/loan/exception/model/ExceptionModel.java
+++ src/main/java/az/kapitalbank/loan/exception/model/ExceptionModel.java
@@ -1,14 +1,30 @@
 package az.kapitalbank.loan.exception.model;
 
+import java.util.Map;
+
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@AllArgsConstructor(staticName = "of")
 public class ExceptionModel {
     String code;
     String message;
+    Map<String, String> checks;
+
+    public static ExceptionModel of(String code, String message) {
+        var object = new ExceptionModel();
+        object.setCode(code);
+        object.setMessage(message);
+        return object;
+    }
+
+    public static ExceptionModel of(String code, String message, Map<String, String> checks) {
+        var object = new ExceptionModel();
+        object.setCode(code);
+        object.setMessage(message);
+        object.setChecks(checks);
+        return object;
+    }
 }
