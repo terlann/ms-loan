4f8d12d9d4957b6daa241f92a55a309ad0880d91
QurbanovTM <Tarlan.Qurbanov@kapitalbank.az>
2022-06-21 16:30:40 +0400

    NPO-587 added new fields

--- src/main/java/az/kapitalbank/loan/dto/LeadLoanRequestDto.java
+++ src/main/java/az/kapitalbank/loan/dto/LeadLoanRequestDto.java
@@ -34,4 +34,6 @@ public class LeadLoanRequestDto {
     ProductType productType;
     SubProductType subProductType;
     FormalizationMethod formalizationMethod;
+    BigDecimal monthlyPayment;
+    String umicoUserId;
 }
