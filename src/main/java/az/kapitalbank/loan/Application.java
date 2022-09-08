89e59ba53381f652a1887e6f5eaef3641b4762d3
malik.mammadov <malik.mammadov@kapitalbank.az>
2022-09-08 11:12:40 +0400

    NPO-874 - consul test

--- src/main/java/az/kapitalbank/loan/Application.java
+++ src/main/java/az/kapitalbank/loan/Application.java
@@ -1,11 +1,24 @@
 package az.kapitalbank.loan;
 
+import az.kapitalbank.loan.constants.TempProps;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    private TempProps tempProps;
+
+    @GetMapping("/key")
+    String getKey() {
+        return tempProps.getKey();
     }
 }
