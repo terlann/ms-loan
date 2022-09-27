08cbe20248ceb86bcd285ab10b7fc247178c691d
tarlan <tarlan.qurbanov@kapitalbank.az>
2022-09-27 14:29:53 +0400

    NPO-902 Comments have been corrected

--- src/main/java/az/kapitalbank/loan/constants/LeadStatus.java
+++ src/main/java/az/kapitalbank/loan/constants/LeadStatus.java
@@ -3,18 +3,22 @@ package az.kapitalbank.loan.constants;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 @Getter
+@ToString
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public enum LeadStatus {
-    WAITING,
-    SENDING,
-    SUCCESS,
-    DELETED,
-    OPTIMUS_REJECTED,
-    OPTIMUS_ERROR,
-    OPTIMUS_ACCEPTED,
-    ERROR;
+    WAITING("waiting"),
+    SENDING("sending"),
+    SUCCESS("success"),
+    DELETED("deleted"),
+    OPTIMUS_REJECTED("optimus_rejected"),
+    OPTIMUS_ERROR("optimus_error"),
+    OPTIMUS_ACCEPTED("optimus_accepted"),
+    ERROR("error");
+
+    String status;
 }
