fe57d9c6452ca81b14422be533c8d90e1c839828
tarlan <tarlan.qurbanov@kapitalbank.az>
2022-11-18 11:13:33 +0400

    NPO-988 changed unit test name

--- src/test/java/az/kapitalbank/loan/controller/LeadLoanControllerTest.java
+++ src/test/java/az/kapitalbank/loan/controller/LeadLoanControllerTest.java
@@ -33,7 +33,7 @@ class LeadLoanControllerTest {
     LeadLoanService leadLoanService;
 
     @Test
-    void addLead_ShouldPass() throws Exception {
+    void addLead_xLeadSourcePresentsInHeader_ShouldResponseOkWithLeadId() throws Exception {
         var request = LeadLoanRequestDto.builder()
                 .phoneNumber(PHONE_NUMBER.getValue())
                 .build();
